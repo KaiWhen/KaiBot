ef77cbac87a000be1edbf3e0cfa55a10c439e2a3
Kevin <wangkw523@gmail.com>
2021-04-05 15:55:08 +0100

    aaeaegeawg

--- mongo.js
+++ mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://dbUser:kxgH7x87PaNVl2Tl@cluster0.99lyy.mongodb.net/kaibotdb?retryWrites=true&w=majority';
+const mongoURI = 'mongodb+srv://dbUser:a3PBuyjqR2n37vj@cluster0.99lyy.mongodb.net/kaibotdb?retryWrites=true&w=majority';
 
 module.exports = async () => {
     await mongoose.connect(mongoURI, {
