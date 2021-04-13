ed6e4c7539604a62d3609e11430ca6ac9e2edbf1
Kevin <wangkw523@gmail.com>
2021-04-13 17:00:48 +0100

    add a play song test command

---
+++ commands/osu_song_guess/playaudiotest.js
@@ -0,0 +1,32 @@
+const ytSearch = require('yt-search');
+const ytdl = require('ytdl-core');
+
+module.exports = {
+	name: 'audiotest',
+	description: 'test',
+	async execute(message, args) {
+		const { voice } = message.member;
+        if(!voice.channelID) return message.reply('you must be in a voice channel.');
+
+		let song = {};
+		const video_finder = async (query) => {
+			const video_result = await ytSearch(query);
+			return (video_result.videos.length > 1) ? video_result.videos[0] : null;
+		};
+
+		const video = await video_finder(args.join(' '));
+		if (video) {
+			song = { title: video.title, url: video.url };
+		} else {
+			message.channel.send('Error finding video.');
+		}
+
+		const connection = await voice.channel.join();
+		const stream = ytdl(song.url, { filter: 'audioonly' });
+		connection.play(stream, { seek: 0, volume: 0.5 })
+		.on('finish', () => {
+			console.log('finished playing');
+		});
+
+	}
+};
