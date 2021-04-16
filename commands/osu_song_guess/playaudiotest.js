70fe9f54c789b644b25a37453e26955d2a485bc9
Kevin <wangkw523@gmail.com>
2021-04-16 11:57:08 +0100

    aowenfoiuna

--- commands/osu_song_guess/playaudiotest.js
+++ commands/osu_song_guess/playaudiotest.js
@@ -9,31 +9,24 @@ module.exports = {
 		const { voice } = message.member;
         if(!voice.channelID) return message.reply('you must be in a voice channel.');
 
-		// let song = {};
-		// const video_finder = async (query) => {
-		// 	const video_result = await ytSearch(query);
-		// 	return (video_result.videos.length > 1) ? video_result.videos[0] : null;
-		// };
+		let song = {};
+		const video_finder = async (query) => {
+			const video_result = await ytSearch(query);
+			return (video_result.videos.length > 1) ? video_result.videos[0] : null;
+		};
 
-		// const video = await video_finder(args.join(' '));
-		// if (video) {
-		// 	song = { title: video.title, url: video.url };
-		// } else {
-		// 	message.channel.send('Error finding video.');
-		// }
+		const video = await video_finder(args.join(' '));
+		if (video) {
+			song = { title: video.title, url: video.url };
+		} else {
+			message.channel.send('Error finding video.');
+		}
 
 		const connection = await voice.channel.join();
-		// const stream = ytdl(song.url, { filter: 'audioonly' });
-		// const dispatcher = connection.play(stream, { seek: 0, volume: 0.5 })
-		// .on('finish', () => {
-		// 	console.log('finished playing');
-		// });
-
-		const audio = path.join(__dirname, 'songtest.mp3');
-		const dispatcher = connection.play(audio, { seek: 0, volume: 0.5 })
+		const stream = ytdl(song.url, { filter: 'audioonly' });
+		const dispatcher = connection.play(stream, { seek: 0, volume: 0.5 })
 		.on('finish', () => {
 			console.log('finished playing');
 		});
-
 	}
 };
