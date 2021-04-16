af738b53d43143c36ef20c1458f8cbdbb975616d
Kevin <wangkw523@gmail.com>
2021-04-16 11:50:52 +0100

    osweinrfoiuni

--- commands/osu_song_guess/playaudiotest.js
+++ commands/osu_song_guess/playaudiotest.js
@@ -9,18 +9,18 @@ module.exports = {
 		const { voice } = message.member;
         if(!voice.channelID) return message.reply('you must be in a voice channel.');
 
-		let song = {};
-		const video_finder = async (query) => {
-			const video_result = await ytSearch(query);
-			return (video_result.videos.length > 1) ? video_result.videos[0] : null;
-		};
+		// let song = {};
+		// const video_finder = async (query) => {
+		// 	const video_result = await ytSearch(query);
+		// 	return (video_result.videos.length > 1) ? video_result.videos[0] : null;
+		// };
 
-		const video = await video_finder(args.join(' '));
-		if (video) {
-			song = { title: video.title, url: video.url };
-		} else {
-			message.channel.send('Error finding video.');
-		}
+		// const video = await video_finder(args.join(' '));
+		// if (video) {
+		// 	song = { title: video.title, url: video.url };
+		// } else {
+		// 	message.channel.send('Error finding video.');
+		// }
 
 		const connection = await voice.channel.join();
 		const stream = ytdl(song.url, { filter: 'audioonly' });
